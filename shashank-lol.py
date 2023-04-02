755d900ce46eee0d6d795b1805c0c506c225f996
Shashank Arora <shashank.a1508@gmail.com>
2023-04-02 19:33:52 +0530

    Task 3 Completed

--- shashank-lol.py
+++ shashank-lol.py
@@ -1,2 +1,3 @@
 print("Breaking Bad")
 print("The show highlights what a chemistry professor does to provide to his family and to utilize his chemistry knowledge")
+print("Learned a lot of things. Amazing experience")
