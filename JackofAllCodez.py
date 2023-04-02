8f864121a10a1fd0d3970e84df81a9785f1d3a64
Jai Khanna <120464272+JackofAllCodez@users.noreply.github.com>
2023-04-02 19:40:46 +0530

    Commit 5

--- JackofAllCodez.py
+++ JackofAllCodez.py
@@ -1,4 +1,3 @@
 print("Steins Gate")
 print("Its story revolves around time travel and as I am huge fan of space and time related things, i really loved this show!")
-print("Jai Khanna💀")
-print("N")
+print("My experince with the FOSS Workshops was great. Learned a lot of things!")
