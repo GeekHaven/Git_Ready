28925061d09f6e0e53ce06a4d86f61c3eb3f7449
Tejas Sharma <tsharma1704@gmail.com>
2023-04-02 19:27:08 +0530

    Commit Message changed successfully

--- Tej-as1.py
+++ Tej-as1.py
@@ -1,8 +1,12 @@
 print("Never Have I ever")
 
 print("Many ups and downs in the movie makes it interesting")
+
 print("Emma Watson")
 print("N")
 
 print("Many ups and downs in the movie makes it interesting")
+
+
+print("Emma Watson")
 
