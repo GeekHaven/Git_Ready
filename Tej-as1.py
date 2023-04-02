b909a8807606366dfc60c3e2fe414ee836de97b5
Tejas Sharma <tsharma1704@gmail.com>
2023-04-02 19:25:42 +0530

    Resolved

--- Tej-as1.py
+++ Tej-as1.py
@@ -1,4 +1,8 @@
 print("Never Have I ever")
+
 print("Many ups and downs in the movie makes it interesting")
 print("Emma Watson")
 print("N")
+
+print("Many ups and downs in the movie makes it interesting")
+
