ad95dcaec9880bef95d21634b58aeda20a4133e7
ishant <ishantkuwt@gmail.com>
2023-04-02 19:08:40 +0530

    commit 3

--- insanecoder02.py
+++ insanecoder02.py
@@ -1,2 +1,3 @@
 Ans1. The Big Bang Theory
 Ans2. I like how an isolated man "sheldon cooper" changes by time without even noticing the changes with the help of his friends.
+Ans3. Anushka Pandit from Sec-C(IT-BI).
